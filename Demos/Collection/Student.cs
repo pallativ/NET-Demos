50ae7d9be81b47577660f26bcd1a69a55fe33b95
Veera Pallati <veera.pallati@abjayon.com>
2021-05-28 12:16:46 +0530

    Add project files.

---
+++ Demos/Collection/Student.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+
+namespace Demos.Collection
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public class Course : IEnumerable
+    {
+        private Student[] students;
+        public Course(Student[] students)
+        {
+            this.students = students;
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return new CourseEnumerator(this.students);
+        }
+    }
+    public class CourseEnumerator : IEnumerator
+    {
+        private readonly Student[] students;
+        private int position = -1;
+
+        public CourseEnumerator(Student[] students)
+        {
+            this.students = students;
+        }
+        public object Current => this.students[position];
+
+        public bool MoveNext()
+        {
+            if (position < this.students.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+}
---
+++ Demos/Program.cs
@@ -0,0 +1,24 @@
+﻿using Demos.Collection;
+using System;
+
+namespace Demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[3];
+            students[0] = new Student() { Id = 1, FirstName = "a", LastName = "aa" };
+            students[1] = new Student() { Id = 2, FirstName = "b", LastName = "bb" };
+            students[2] = new Student() { Id = 2, FirstName = "c", LastName = "cc" };
+            var enumerator = new Course(students);
+            foreach (Student student in enumerator)
+            {
+                Console.WriteLine($"Id:{student.Id}");
+                Console.WriteLine($"First Name:{student.FirstName}");
+                Console.WriteLine($"Last Name:{student.LastName}");
+                Console.WriteLine("................");
+            }
+        }
+    }
+}
